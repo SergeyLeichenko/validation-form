b9d5daf5af7881253db8ec6d16cc291af2ea0f3c
Serhii Leichenko <leichenko@chmsoft.com.ua>
2024-01-22 22:24:56 +0200

    form validation

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mx-auto">
+    <main class="min-h-screen bg-[#1a1a1a]">
+      <section class="px-5 py-24 text-gray-400">
+
+        <FormRegistration />
+
+      </section>
+    </main>
+  </div>
+</template>
